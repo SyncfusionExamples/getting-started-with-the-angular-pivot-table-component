c6b0266a505fdf909fdb104a8993d21989f6cfde
SrisabariSF4261 <srisabari.venkatesan@syncfusion.com>
2024-07-08 18:27:24 +0530

    Updated the getting started sample in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,43 @@
-import { NgModule } from '@angular/core'
-import { BrowserModule } from '@angular/platform-browser'
-import { PivotViewAllModule, PivotFieldListAllModule } from '@syncfusion/ej2-angular-pivotview'
+import { Component, OnInit } from '@angular/core';
+import { PivotViewModule , PivotFieldListModule} from '@syncfusion/ej2-angular-pivotview';
+import { IDataOptions, IDataSet,FieldListService, CalculatedFieldService } from '@syncfusion/ej2-angular-pivotview';
 
-
-
-import { Component } from '@angular/core';
-import { IDataOptions, IDataSet, PivotView, FieldListService, CalculatedFieldService } from '@syncfusion/ej2-angular-pivotview';
-import { Pivot_Data } from './datasource';
 
 @Component({
-imports: [
-        
-        PivotViewAllModule,
-        PivotFieldListAllModule
-    ],
-
-
-standalone: true,
+  imports: [    
+     PivotViewModule,
+     PivotFieldListModule
+],
+standalone:true,
   selector: 'app-container',
   providers: [FieldListService, CalculatedFieldService],
-  // specifies the template string for the pivot table component
   template: `<div style="height: 480px;"><ejs-pivotview #pivotview id='PivotView' height='350' [dataSourceSettings]=dataSourceSettings [width]=width allowCalculatedField='true' showFieldList='true'></ejs-pivotview></div>`
 })
-
-export class AppComponent {
-    public dataSourceSettings?: IDataOptions;
+export class AppComponent implements OnInit {
+    public pivotData!: IDataSet[];
+    public dataSourceSettings!: IDataOptions;
     public width?: string;
     ngOnInit(): void {
+
+        this.pivotData = [
+          { Amount: 2100, Country: "Canada", Date: "FY 2005", Product: "Bike", Quantity: 22, State: "Alberta" },
+          { Amount: 1100, Country: "Canada", Date: "FY 2006", Product: "Van", Quantity: 32, State: "Quebec" },
+          { Amount: 3100, Country: "Canada", Date: "FY 2007", Product: "Car", Quantity: 22, State: "Alberta" },
+          { Amount: 4800, Country: "France", Date: "FY 2005", Product: "Bike", Quantity: 49, State: "Charente-Maritime" },
+          { Amount: 9100, Country: "France", Date: "FY 2007", Product: "Car", Quantity: 64, State: "Charente-Maritime" },
+          { Amount: 2350, Country: "France", Date: "FY 2008", Product: "Van", Quantity: 46, State: "Gers" },
+          { Amount: 3400, Country: "Germany", Date: "FY 2005", Product: "Bike", Quantity: 78, State: "Bayern" },
+          { Amount: 8400, Country: "Germany", Date: "FY 2006", Product: "Van", Quantity: 90, State: "Hamburg" },
+          { Amount: 7200, Country: "Germany", Date: "FY 2008", Product: "Car", Quantity: 92, State: "Bayern" },
+          { Amount: 1040, Country: "United Kingdom", Date: "FY 2005", Product: "Bike", Quantity: 47, State: "England" },
+          { Amount: 1500, Country: "United Kingdom", Date: "FY 2006", Product: "Van", Quantity: 24, State: "England" },
+          { Amount: 4820, Country: "United Kingdom", Date: "FY 2008", Product: "Car", Quantity: 72, State: "England" },
+          { Amount: 1520, Country: "United States", Date: "FY 2006", Product: "Bike", Quantity: 53, State: "North Carolina" },
+          { Amount: 3320, Country: "United States", Date: "FY 2007", Product: "Car", Quantity: 49, State: "South Carolina" },
+          { Amount: 6300, Country: "United States", Date: "FY 2008", Product: "Van", Quantity: 45, State: "South Carolina" },];
+
         this.dataSourceSettings = {
-            dataSource: Pivot_Data as IDataSet[],
+            dataSource: this.pivotData,
             columns: [{ name: 'Date', caption: 'Date' }, { name: 'Product' }],
             expandAll: false,
             enableSorting: true,
@@ -42,5 +51,3 @@ export class AppComponent {
         this.width = "100%";
     }
 }
-
-
